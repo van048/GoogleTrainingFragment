ab44ed1f1069b4c9beb28c2c8e48f632dfb92d43
van048 <yybbenyang@gmail.com>
2016-10-09 10:47:46 +0800

    Creating a Fragment

--- app/src/main/java/cn/ben/googletrainingfragment/MainActivity.java
+++ app/src/main/java/cn/ben/googletrainingfragment/MainActivity.java
@@ -1,13 +1,21 @@
 package cn.ben.googletrainingfragment;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import cn.ben.googletrainingfragment.fragment.ArticleFragment;
+
+public class MainActivity extends AppCompatActivity implements ArticleFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.news_article);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
